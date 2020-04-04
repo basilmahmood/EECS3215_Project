51657c19387e3a935fe29da5d2d612de83f93bc4
Basil Mahmood <basilmahmood98@gmail.com>
2020-04-03 23:26:25 -0400

    Added front-end

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const SerialPort = require('serialport');
 const express = require('express');
 const app = express();
+var path = require('path');
 const Delimiter = require('@serialport/parser-delimiter')
 
 const port = new SerialPort('COM3', {
@@ -11,11 +12,16 @@ const parser = port.pipe(new Delimiter({ delimiter: '\n' }))
 let temp;
 
 parser.on('data', (buffer) => {
-  temp = parseFloat(buffer.toString())/1000;
   console.log(buffer.toString());
+  const tempString = buffer.toString();
+  temp = parseFloat(tempString)/(Math.pow(10, 3 + tempString.length - 5)); // Ensures 2 numbers after the decimal point incase of error
 }) 
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/temp', (req, res) => {
   res.send(temp.toString());
 });
  
